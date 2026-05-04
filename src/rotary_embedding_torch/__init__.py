50fe2ac33056ed4a8c10ccf1a8a9949d1c3029a6
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-05-04 09:32:33 -0500

    typing. deduplication. flow. status messages.
    - more type annotations.
    - replace common functions with torch_einops_kit.
    - hunterMakesPy.raiseIfNone for type narrowing. - some formatting.
    - change flow to deduplicate.
    - change assert to raise and improve messages.
    - Remove inline comments from Captain Obvious.
    - use class attributes instead of initialization parameters.
    - prevent corner-case error with a warning.
    - remove unnecessary check.

--- src/rotary_embedding_torch/__init__.py
+++ src/rotary_embedding_torch/__init__.py
@@ -1,8 +1,6 @@
-from rotary_embedding_torch.rotary_embedding_torch import (
-    RotaryEmbedding,
-    apply_learned_rotations,
-    apply_rotary_emb,
-    broadcast,
-)
+from __future__ import annotations
 
-__all__ = ["RotaryEmbedding", "apply_learned_rotations", "apply_rotary_emb", "broadcast"]
+from rotary_embedding_torch.rotary_embedding_torch import (
+    apply_learned_rotations, apply_rotary_emb, broadcast_cat as broadcast, RotaryEmbedding)
+
+__all__ = ["RotaryEmbedding", "apply_learned_rotations", "apply_rotary_emb"]
